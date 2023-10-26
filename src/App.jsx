577fb18543b6f7a6d21b966ee9ee87d91ae1dbb6
kristof1345 <kristofnemeth1345@gmail.com>
2023-10-26 21:20:48 +0200

    ok

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Header from "../src/components/Header";
+
+function App() {
+  return (
+    <>
+      <main id="dashboard">
+        <Header />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div id="header">
+      <div className="brand">Booky</div>
+      <div className="add-new-book-holder">
+        <button>Add New Book</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
